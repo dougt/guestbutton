316d8885d8acc6008fc9a47122930098009767a1
Joan Perals Tresserra <j.pertres@gmail.com>
2014-01-30 16:06:02 +0100

    First functional, but visually terrible version

--- js/app.js
+++ js/app.js
@@ -1,18 +1,42 @@
 var app = app || {};
 
 (function() {
-    var settings = window.navigator.mozSettings;
+    var option = 'bluetooth',
+        settings = window.navigator.mozSettings;
     if (!settings) {
         console.log('no settings');
         return;
     }
-    var reqEnabled = settings.createLock().get('bluetooth.enabled');
+    console.log('loading');
+    var reqEnabled = settings.createLock().get(option + '.enabled');
     reqEnabled.onsuccess = function () {
-        var isBluetoothEnabled = reqEnabled.result['bluetooth.enabled'];
-        console.log('AAAAAAAAA: ' + isBluetoothEnabled);
+        var isSettingEnabled = reqEnabled.result[option + '.enabled'];
+        console.log('Setting is enabled: ' + isSettingEnabled);
+        if(isSettingEnabled) {
+            app.displayLockScreen();
+        }
+        else {
+            app.setOption(true);
+        }
     };
-    app.setOption = function(value) {
-        var reqSet = settings.createLock().set({'bluetooth.enabled': value});
-        window.close();
+    app.displayLockScreen = function() {
+        document.body.style.display = 'block';
+        var buttonDisable = document.getElementById('button-disable');
+        buttonDisable.onclick = function() {
+            app.setOption(false);
+        };
+        var buttonCancel = document.getElementById('button-cancel');
+        buttonCancel.onclick = function() {
+            window.close();
+        };
     }
+    app.setOption = function(value) {
+        console.log('setOption');
+        var reqSet = settings.createLock().set({'bluetooth.enabled': value});
+        console.log('we are going to close in a moment');
+        reqSet.onsuccess = function () {
+            console.log('close!');
+            window.close();
+        };
+    };
 })();
